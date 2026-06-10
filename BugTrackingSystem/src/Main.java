e5d463f70738edfa97f3121fa77d446302af14fa
sriyathid-commits <sriyathid@gmail.com>
2026-06-10 17:03:27 +0530

    Create Main.java

---
+++ BugTrackingSystem/src/Main.java
@@ -0,0 +1,8 @@
+package BugTrackingSystem.src;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bug Tracking System started...");
+        // Future: Add bug reporting and tracking logic here
+    }
+}
